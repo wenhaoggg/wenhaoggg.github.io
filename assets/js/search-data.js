10ed8d8bf5b81038fd2252d04b02db240b1723c7
wenhaoggg <74142574+wenhaoggg@users.noreply.github.com>
2025-10-14 16:21:51 +0000

    Deploying to gh-pages from @ wenhaoggg/wenhaoggg.github.io@e4301b7821631e787310819a3e761658dc98303b 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -30,28 +30,7 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/cv/";
           },
-        },{id: "nav-people",
-          title: "people",
-          description: "members of the lab or group",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/people/";
-          },
-        },{id: "dropdown-bookshelf",
-              title: "bookshelf",
-              description: "",
-              section: "Dropdown",
-              handler: () => {
-                window.location.href = "/books/";
-              },
-            },{id: "dropdown-blog",
-              title: "blog",
-              description: "",
-              section: "Dropdown",
-              handler: () => {
-                window.location.href = "/blog/";
-              },
-            },{id: "post-ml-metrics",
+        },{id: "post-ml-metrics",
         
           title: "Ml Metrics",
         
