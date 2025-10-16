37bc7cff8dc960dd6a7f2431dbb7a068c75ab757
wenhaoggg <74142574+wenhaoggg@users.noreply.github.com>
2025-10-16 00:10:54 +0000

    Deploying to gh-pages from @ wenhaoggg/wenhaoggg.github.io@14f9a447f0d2a0d0cf525d2f49f9bbf585a81d28 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -30,7 +30,18 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/cv/";
           },
-        },{id: "news-a-simple-inline-announcement",
+        },{id: "post-docker-basics",
+        
+          title: "Docker Basics",
+        
+        description: "Docker tips",
+        section: "Posts",
+        handler: () => {
+          
+            window.location.href = "/blog/2025/Docker_Basics/";
+          
+        },
+      },{id: "news-a-simple-inline-announcement",
           title: 'A simple inline announcement.',
           description: "",
           section: "News",},{id: "news-a-long-announcement-with-details",
